d711238f0e3d7133b29b313d7382412d76a43a3f
Jev IRIE PIXEL <jev@iriepixel.com>
2024-05-31 23:03:25 +0100

    Initial Commit

---
+++ IrieGamesDB/IrieGamesDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrieGamesDBApp.swift
+//  IrieGamesDB
+//
+//  Created by Jev Smolnikov on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct IrieGamesDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
